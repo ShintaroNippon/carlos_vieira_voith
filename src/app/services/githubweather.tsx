66877fcb7316dd8058f26c00ab68d8f616ceb805
shintaro <carlos.vieira@trekksoft.com>
2021-11-16 13:46:09 +0000

    update data showed

--- src/app/services/githubweather.tsx
+++ src/app/services/githubweather.tsx
@@ -1,11 +1,13 @@
 class VoithGetWeather {
   async getVoithGetWeather() {
+    // GET CITY CODE with:
+    /* get city code with "Reference": "/locations/v1/cities/countryCode/search?apikey=AqdQoGW7ICWkluCAV3N9sjGKJqjNBmNs&q=Heidenheim" */
     const url =
       'https://dataservice.accuweather.com/forecasts/v1/daily/1day/172768.json?language=en&apikey=AqdQoGW7ICWkluCAV3N9sjGKJqjNBmNs';
 
     const response = await fetch(url);
     const data = await response.json();
-    return data.DailyForecasts[0];
+    return data;
   }
 }
 
